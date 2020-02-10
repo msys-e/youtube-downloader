5b5ccd67623eb16649d5cd7569aef12b768c79c1
msys-e <attainall1@gmail.com>
2020-02-11 01:27:30 +0900

    add: typescript environment

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+        'stylelint-config-rational-order',
+        'stylelint-prettier/recommended',
+    ],
+    rules: {
+        'string-quotes': 'single',
+    }
+};
