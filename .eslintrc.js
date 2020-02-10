bf61e6d7b28981fe3a7baf48c211aefaa23dd34a
msys-e <attainall1@gmail.com>
2020-02-11 01:56:17 +0900

    update: eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,26 @@
 module.exports = {
-　　env: {
-        es6: true,
+    parser: "@typescript-eslint/parser",
+    extends: [
+      "plugin:@typescript-eslint/recommended",
+      "prettier/@typescript-eslint",
+      "plugin:prettier/recommended" 
+    ],
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module"
     },
-    extends: ['google', 'plugin:prettier/recommended'],
-};
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "all",
+          endOfLine: "lf",
+          semi: true,
+          singleQuote: true,
+          printWidth: 80,
+          tabWidth: 4
+        }
+      ]
+    },
+  };
+  
--- .stylelintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: [
-        'stylelint-config-rational-order',
-        'stylelint-prettier/recommended',
-    ],
-    rules: {
-        'string-quotes': 'single',
-    }
-};
